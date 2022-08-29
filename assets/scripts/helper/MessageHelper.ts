f5fcaef456a6ce0f3c5a9f4e2c921a42ef9beba4
ruslan <sonikx94@yandex.ru>
2022-08-29 14:36:23 +0300

    Поправил формат сообщения

--- assets/scripts/helper/MessageHelper.ts
+++ assets/scripts/helper/MessageHelper.ts
@@ -2,14 +2,14 @@ import { IMessageInfo } from "../entity/IMessageInfo";
 
 export class MessageHelper {
     static parseMessage(input: any, actualUserId: number): IMessageInfo {
-        let userId: number = input["user"]["id"];
+        let userId: number = input["user_id"];
         let isUser: boolean = userId == actualUserId;
 
         let data: IMessageInfo = {
             message: input["text"],
             sendTime: input["send_time"],
             userId: userId,
-            username: isUser ? "Me" : input["user"]["username"],
+            username: isUser ? "Me" : input["username"],
             isUser: isUser
         };
 
