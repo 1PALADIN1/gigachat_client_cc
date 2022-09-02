5071fbad809352621730e56ceb94e0686132e02d
ruslan <sonikx94@yandex.ru>
2022-09-02 16:38:33 +0300

    Конвертация UTC в локальное время

--- assets/scripts/helper/MessageHelper.ts
+++ assets/scripts/helper/MessageHelper.ts
@@ -4,10 +4,11 @@ export class MessageHelper {
     static parseMessage(input: any, actualUserId: number): IMessageInfo {
         let userId: number = input["user_id"];
         let isUser: boolean = userId == actualUserId;
+        let utcTime: Date = new Date(input["send_time"] + " UTC");
 
         let data: IMessageInfo = {
             message: input["text"],
-            sendTime: input["send_time"],
+            sendTime: utcTime.toLocaleString(),
             userId: userId,
             username: isUser ? "Me" : input["username"],
             isUser: isUser
